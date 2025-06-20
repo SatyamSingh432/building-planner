9bfd7758b28265000c0087960f647e28815b9f28
Satyam Singh <satyamrajput7239@gmail.com>
2025-06-20 20:14:40 +0530

    feat: Add ability to draw rectange

--- src/component/Canvas.jsx
+++ src/component/Canvas.jsx
@@ -1,16 +1,61 @@
+import { useState } from "react";
 import { Stage, Layer, Rect, Text } from "react-konva";
 import { useDrawing } from "../context/DrawingContext";
 
 const Canvas = () => {
-  const { shapes, annotationsVisible } = useDrawing();
-
+  const { shapes, annotationsVisible, tool, setShapes } = useDrawing();
+  const [newShape, setNewShape] = useState(null);
   const stageHeight = window.innerHeight;
   const stageWidth = window.innerWidth;
 
+  const handleMouseDown = (e) => {
+    const stage = e.target.getStage();
+    const point = stage.getPointerPosition();
+    console.log({ point });
+    if (tool === "rect") {
+      setNewShape({
+        type: "rect",
+        x: point.x,
+        y: point.y,
+        width: 0,
+        height: 0,
+      });
+    }
+  };
+
+  const handleMouseMove = (e) => {
+    if (!newShape) return;
+    const stage = e.target.getStage();
+    const point = stage.getPointerPosition();
+
+    console.log("move", point);
+
+    if (newShape.type === "rect") {
+      setNewShape((prev) => ({
+        ...prev,
+        width: point.x - prev.x,
+        height: point.y - prev.y,
+      }));
+    }
+  };
+
+  const handleMouseUp = () => {
+    if (newShape) {
+      setShapes([...shapes, newShape]);
+      setNewShape(null);
+    }
+  };
+
   return (
-    <Stage width={stageWidth} height={stageHeight}>
+    <Stage
+      width={stageWidth}
+      height={stageHeight}
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+    >
       <Layer>
-        {shapes.map((shape, i) => {
+        {[...shapes, ...(newShape ? [newShape] : [])].map((shape, i) => {
           return (
             <>
               <Rect
